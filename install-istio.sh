120df60d3003e73065ae9d18a0a3f434e09645c8
Lin Sun <lin.sun@solo.io>
2025-05-12 17:23:28 -0400

    update to  latest istio

--- install-istio.sh
+++ install-istio.sh
@@ -4,13 +4,13 @@
 # TAG=1.24-alpha.6a458274241dd51fbd6e015e7f439a29e8beb07f
 # istioctl install --set tag=$TAG --set hub=gcr.io/istio-testing --set profile=ambient --skip-confirmation  --set meshConfig.accessLogFile=/dev/stdout --set values.pilot.env.PILOT_ENABLE_IP_AUTOALLOCATE=true --set  values.cni.ambient.dnsCapture=true
 
-istioctl install --set profile=ambient --skip-confirmation  --set meshConfig.accessLogFile=/dev/stdout --set values.pilot.env.PILOT_ENABLE_IP_AUTOALLOCATE=true --set  values.cni.ambient.dnsCapture=true
+istioctl install --set profile=ambient --skip-confirmation  --set meshConfig.accessLogFile=/dev/stdout
 
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
   kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/standard-install.yaml
 
 # change this to your istio dir
-ISTIO_DOWNLOAD_DIR=~/Downloads/istio-1.25.0
+ISTIO_DOWNLOAD_DIR=~/Downloads/istio-1.26.0
 
 kubectl apply -f $ISTIO_DOWNLOAD_DIR/samples/addons/prometheus.yaml
 kubectl apply -f $ISTIO_DOWNLOAD_DIR/samples/addons/kiali.yaml
