e32e542239ad886d7c2bd1b094e9df37ad9ab12c
Lin Sun <lin.sun@solo.io>
2025-06-24 21:41:58 -0600

    Revert "add ingress gateway"
    This reverts commit 7d73d8e296568bf7ace51676049d87eae15014c6.

--- cleanup-before-demo.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f policy/
-kubectl label namespace default istio.io/dataplane-mode-
-istioctl waypoint delete waypoint 
-kubectl delete pod --all -n istio-egress
-kubectl label namespace default istio.io/use-waypoint-
